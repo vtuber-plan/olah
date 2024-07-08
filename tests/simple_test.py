b56d0b2eb5f2b52e52d331fd38e71ef394c50c2d
jstzwj <1103870790@qq.com>
2024-07-09 00:04:05 +0800

    update tests

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -4,12 +4,22 @@ import time
 
 from huggingface_hub import snapshot_download
 
-def test_simple():
+def test_dataset():
     process = subprocess.Popen(['python', '-m', 'olah.server'])
 
     os.environ['HF_ENDPOINT'] = 'http://localhost:8090'
     snapshot_download(repo_id='Nerfgun3/bad_prompt', repo_type='dataset',
                     local_dir='./dataset_dir', max_workers=8)
+
+    # 终止子进程
+    process.terminate()
+
+def test_model():
+    process = subprocess.Popen(['python', '-m', 'olah.server'])
+
+    os.environ['HF_ENDPOINT'] = 'http://localhost:8090'
+    snapshot_download(repo_id='prajjwal1/bert-tiny', repo_type='model',
+                    local_dir='./model_dir', max_workers=8)
 
     # 终止子进程
     process.terminate()
